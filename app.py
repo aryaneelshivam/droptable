50943cc2f8de83cb1f1e6102bbaf8ddefd819b96
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-06 16:31:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def main():
                 @st.cache_resource
                 def get_pyg_html(df: pd.DataFrame) -> str:
                     # When you need to publish your application, you need set `debug=False`,prevent other users to write your config file.
-                    html = get_streamlit_html(df, theme="light", use_kernel_calc=True, debug=False)
+                    html = get_streamlit_html(df, dark="light", use_kernel_calc=True, debug=False)
                     return html
 
                 components.html(get_pyg_html(df), width=1240, height=915)
