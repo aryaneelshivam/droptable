1e19adc7585502119aa314cc5b068cd8e6fe2167
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-06 16:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def main():
                     html = get_streamlit_html(df, use_kernel_calc=True, debug=False)
                     return html
 
-                components.html(get_pyg_html(df), width=1240, height=915)
+                components.html(get_pyg_html(df), width=1240, height=915, dark="light")
 
 if __name__ == "__main__":
     main()
