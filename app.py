0cb729e9730293d203f3db1e5673be46ed24cb42
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-06 17:28:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,6 +106,9 @@ def main():
                     return html
 
                 components.html(get_pyg_html(df), width=1240, height=915)
+                new_dfs, code = spreadsheet(CSV_URL)
+                st.write(new_dfs)
+                st.code(code)
 
 if __name__ == "__main__":
     main()
