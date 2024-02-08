a609a45a74f4e582ddbe88e808c726aaff2a07ad
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-08 17:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ init_streamlit_comm()
 def load_data(file):
     data = pd.read_csv(file, encoding='latin-1')
     return data
-@st.cache    
+   
 def main():
     selected = option_menu(
         menu_title = None,
