f26d0c6fa2812d092a047575981c7be80639d4b6
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-09 22:03:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,13 +143,14 @@ def main():
                 components.html(get_pyg_html(df), width=1240, height=915)
             with tab2:
                input = st.file_uploader("Choose a CSV file", type=['png'])
-               img = Image.open(input)
-               Google = 'AIzaSyDtl-9-hd5-JIXTnrYhf57_lQKsXm3Ksp0'
-               genai.configure(api_key=Google)
-               model = genai.GenerativeModel('gemini-pro-vision')
-               response = model.generate_content(["Read and analyse this graphs and state all the valuable business intelligence insights one can derive from it", img])
-               st.image(input)
-               st.write(response.text)
+               if input is not None:
+                   img = Image.open(input)
+                   Google = 'AIzaSyDtl-9-hd5-JIXTnrYhf57_lQKsXm3Ksp0'
+                   genai.configure(api_key=Google)
+                   model = genai.GenerativeModel('gemini-pro-vision')
+                   response = model.generate_content(["Read and analyse this graphs and state all the valuable business intelligence insights one can derive from it", img])
+                   st.image(input)
+                   st.write(response.text)
 
 if __name__ == "__main__":
     main()
