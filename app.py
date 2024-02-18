05c5dfa2580d74f8055ab59c24b3158717736c75
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-18 10:22:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def main():
     # the marquee text content
     'content': 'here is custom marquee component~',
     # the marquee container width
-    'width': '600px',
     # the marquee container line height
     'lineHeight': "35px",
     # the marquee duration
