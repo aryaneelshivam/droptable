f504f94feabbfa995e0202dbf239ab5e866b108e
aryaneelshivam <149952480+aryaneelshivam@users.noreply.github.com>
2024-02-23 08:15:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -179,13 +179,16 @@ def main():
             genai.configure(api_key=Google)
             model = genai.GenerativeModel('gemini-pro-vision')
             with st.spinner("Sending and fetching data through DropAI"):
-                response = model.generate_content(["Read and analyse this graphs and state all the valuable business intelligence insights one can derive from it in english", img])
-                st.success("Analysis done 👌")
-                st.toast('Hooray!', icon='🎉')
-                st.balloons()
-                st.write(response.prompt_feedback)
-                st.image(input)
-                st.markdown(response.text)
+                if response = model.generate_content(["Read and analyse this graphs and state all the valuable business intelligence insights one can derive from it in english", img]):
+                    st.success("Analysis done 👌")
+                    st.toast('Hooray!', icon='🎉')
+                    st.balloons()
+                    st.write(response.prompt_feedback)
+                    st.image(input)
+                    st.markdown(response.text)
+                else:
+                    st.write(response.prompt_feedback)
+            
 
 if __name__ == "__main__":
     main()
